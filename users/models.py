491a990fe3128fff0d05cf5c0946f79fa1b8fe41
ildar_safarov <ildarsafarov555@gmail.com>
2026-04-28 18:44:12 +0300

    Create custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from users.managers import UserManager
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email",
+    )
+
+    telegram_chat_id = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name="Telegram chat ID",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
